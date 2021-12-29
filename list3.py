a19af681f401d09d7e45a3e3de4cc01284dd05f0
“yatinsolanki” <yatinsolanki98@gmail.com>
2021-12-29 18:43:25 +0530

    commit 3rd

--- list3.py
+++ list3.py
@@ -3,3 +3,13 @@
 msg=['Python', 2,'ANIL DOLLOR', 5.4]
 
 print(msg)
+
+print('I am learning '+msg[0]+' from '+msg[2]+' sir.')
+
+
+#variable substitution technique
+
+a=10
+name = "ANIL DOLLOR"
+
+print(f"{name } having {a } of experience")
